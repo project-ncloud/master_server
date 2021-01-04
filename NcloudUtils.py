7067b89a0747c94177167c353db1f62be89fd529
Sourav Gain <souravgain605@gmail.com>
2021-01-04 22:39:18 +0530

    added two functions to check pi status

--- NcloudUtils.py
+++ NcloudUtils.py
@@ -1,6 +1,7 @@
 from db         import Mongo
 from os         import getenv
 from helper     import *
+from requests   import get
 
 def getNcloudConfig(DB:Mongo):
     data:dict = DB.get_doc({}, getenv('ADMIN_COLLECTION'))
@@ -19,3 +20,33 @@ def getServers(DB:Mongo):
     block:dict = DB.get_docs({}, getenv('SERVER_COLLECTION'))
     NN = Nas(block)
     return NN.getBlock()
+
+
+def allAlive(DB:Mongo):
+    servers:dict = getServers(DB)
+    counter = 0
+    for server in servers:
+        try:
+            res = get(f'http://{server.get("address")}/init/')
+
+            if res.ok == False:
+                raise Exception
+
+        except Exception as e:
+            print(f'Error ocurred :: {server.get("address")} is not alive\t\t[{server.get("name")}]')
+            return False
+    
+    return True
+
+def isServerAlive(server:dict):
+    try:
+        res = get(f'http://{server.get("address")}/init/')
+
+        if res.ok == False:
+            raise Exception
+
+    except Exception as e:
+        print(f'Error ocurred :: {server.get("address")} is not alive\t\t[{server.get("name")}]')
+        return False
+
+    return True
