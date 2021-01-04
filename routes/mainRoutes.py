a865eb2eec601e796055b1e67c75c90c10619745
Sourav Gain <souravgain605@gmail.com>
2021-01-04 22:45:27 +0530

    /api/user/ DELETE method updated

--- routes/mainRoutes.py
+++ routes/mainRoutes.py
@@ -122,28 +122,42 @@ def usersOps():
 
 @app.route('/api/user/', methods = ['POST', 'DELETE'])
 def userOps():
+    # Assigning only json data
     req = request.json
 
+    # Checking if the client sent json data with the request.
+    if req == None:
+        return allowCors(jsonify({"msg" : "No JSON Data found"}),400)
 
+    # POST - Approve Users from the pending section
+    # Rules - If <addall_inNewHosts> True then server will going to add the user into all the PiServer
     if request.method == 'POST':
         
         #Approve the user
         msg, ret = approveReq(req.get('username'), app.DB)
         if ret == True:
+
+            # Fetching Ncloud configs from the database
             config:dict = getNcloudConfig(app.DB)
 
             if config.get('addall_inNewHosts') == True:
+
+                # Fetching all Pi Server_data from the database
                 servers = getServers(app.DB)
-                operationStatus:int = 0
+
+                # Decision variable to ensure that all the operations are executing successfully
+                # 0 = No Error | 0 < = Error
+                operationStatus = 0
+
+                # Looping through the server data to add the username into each server's host
                 for server in servers:
                     hosts = server.get('hosts')
                     for host in hosts:
-                        '''
-                        Pending Task
-                        call a request to node server to add user and send the sucess respose
-                        '''
                         try:
-                            res = requests.post(f'http://{server.get("address")}/<Pending>')
+                            # Sending request to the Pi Server to add the username to all of it's hosts
+                            res = requests.post(f'http://{server.get("address")}/Pending', json = {
+                                "username" : req.get("username")
+                            })
 
                             if res.ok:
                                 print(f'{req.get("username")} added into {host.get("name")}')
@@ -163,6 +177,76 @@ def userOps():
             print("Error ocurred :: ", msg)
             return allowCors(jsonify({"msg" : "Failed while aproving"}),401)
 
-        #Send the user data to node
+    # Delete User
     else:
-        return 'Delete Hello'
+
+        # User is only available in pending section.
+        # So we dont need to handle the main user collection while removing user
+        if req.get("type").lower() == "pending":
+            msg, ret = rejectReq(req.get('username'), app.DB)
+            if ret == True:
+                return allowCors(jsonify({"msg": "Successfully removed"}))
+            else:
+                return allowCors(jsonify({"msg": "Error ocurred while removing the user"}),400)
+        
+        # Deleting username from main line
+        elif req.get("type").lower() == "normal":
+
+            # Check if all the servers are running or not.
+            # Its very important to make sure that every server is running. 
+            # Otherwise it will create conflict with the database
+            if allAlive(app.DB) == False:
+                return allowCors(jsonify({"msg": "All the servers have to be running while deleting user from main line"}),401)
+
+
+            # Fetching all Pi Server_data from the database
+            servers = getServers(app.DB)
+
+            # Decision variable to ensure that all the operations are executing successfully
+            # 0 = No Error | 0 < = Error
+            operationStatus = 0
+
+            # Looping through the server data to add the username into each server's host
+            for server in servers:
+                hosts = server.get('hosts')
+
+                for host in hosts:
+                    validUsers = host.get('validUsers')
+                    isExists = req.get('username').lower() in validUsers
+
+                    if isExists == True:
+                        try:
+                            res = requests.get(f'http://{server.get("address")}/Pending/', json = {
+                                "username" : req.get('username'),
+                            })
+
+                            if res.ok:
+                                validUsers.remove(req.get("username"))
+                                print(f'{req.get("username")} removed from {host.get("name")}')
+                            else:
+                                raise Exception
+                        except Exception as e:
+                            operationStatus += 1
+                            print(f'{req.get("username")} failed while removing from {host.get("name")}')
+                host.setdefault('validUsers', validUsers)
+                app.DB.update_doc({"name" : server.get('name')}, {"hosts" : hosts}, getenv('SERVER_COLLECTION'))
+                
+
+            # If some targets are failed, we will not remove the username from main line database,
+            # Because there are maybe some Pi Servers turned off and if we remove username in that situation, it will create unstable situation
+            if operationStatus != 0 :
+                return allowCors(jsonify({"msg": "Some targets are failed"}),400)
+            
+            # Removing Username from main line database
+            # Because we know that all the servers are on and all removed the username from their servers
+            ret, dummy = app.DB.remove({"username" : req.get('username')}, getenv('USER_COLLECTION'))
+
+            if ret:
+                return allowCors(jsonify({"msg": "Operation successful"}))
+            else:
+                return allowCors(jsonify({"msg": "Error ocurred while removing user from db"}))
+                
+        else:
+            return allowCors(jsonify({"msg": "Bad request"}),400)
+
+
